217e2b94e44bde2694c1fdca41e1fe56a2018887
NewtonOmbese <newtonombese1@gmail.com>
2022-05-15 07:53:00 +0300

    enabling selections in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,31 @@ import { questionsData } from "./questions.js";
 
 const quiz = document.getElementById("quiz");
 const answerR = document.querySelectorAll(".answer");
+const questionH = document.getElementById("question");
+const answer_a = document.getElementById("answer_a");
+const answer_b = document.getElementById("answer_b");
+const answer_c = document.getElementById("answer_c");
+const answer_d = document.getElementById("answer_d");
+const submitBtn = document.getElementById("submit");
 
+let currentQuiz = 0;
+
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    
+    deselectAnswers();
+
+    const currentQuizData = questionsData[currentQuiz];
+    
+    questionH.innerText = currentQuizData.question;
+    answer_a.innerText = currentQuizData.a;
+    answer_b.innerText = currentQuizData.b;
+    answer_c.innerText = currentQuizData.c;
+    answer_d.innerText = currentQuizData.d;
+
+    
+
+}
