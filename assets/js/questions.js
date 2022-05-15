01830f53f660e8d52ce8bea73e5f9430adbfe3ba
NewtonOmbese <newtonombese1@gmail.com>
2022-05-15 07:32:39 +0300

    adding a number of questions to test out if it is functional

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+export const questionsData = [
+    {
+        question: "What is SAAS in full?",
+        a: "Service As A Support",
+        b: "Software As A Service",
+        c: "Software As A Stock",
+        d: "Serve As A Server",
+        correct: "b",
+    },
+    {
+        question: "Who  invented the compiler?",
+        a: "Charles Babbage",
+        b: "Bill Gates",
+        c: " Grace Hopper",
+        d: "Edith Clarke",
+        correct: "c",
+    },
+    {
+        question: "Which language is used to provide functionality of a website?",
+        a: "Javascript",
+        b: "Java",
+        c: "SQL",
+        d: "HTML",
+    },
+];
